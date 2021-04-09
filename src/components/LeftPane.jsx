383d3c467a1c1c41b597424d79393e72f8caa414
Daksh Khetan <2012daksh@gmail.com>
2021-04-09 20:11:09 +0530

    left-pane complete

--- src/components/LeftPane.jsx
+++ src/components/LeftPane.jsx
@@ -1,9 +1,45 @@
 import React from "react";
+import { useHistory, Link } from "react-router-dom";
+import { Container, VStack, Box } from "@chakra-ui/react";
+import { FaHome, FaUser } from "react-icons/fa";
+import { GoGraph } from "react-icons/go";
+import { FiSettings, FiExternalLink } from "react-icons/fi";
 
 const LeftPane = () => {
+  let history = useHistory();
+
   return (
     <>
-      <div>Left Pane</div>
+      <Container pos="fixed" top="0" left="0" w="7%" h="100%" bg="gray.100">
+        <VStack spacing="50px">
+          <Box mt="100px" w="40px" h="40px">
+            <FaHome
+              size="2.5rem"
+              onClick={() => history.push("/")}
+              style={{ cursor: "pointer" }}
+            />
+          </Box>
+          <Box w="40px" h="40px">
+            <FaUser
+              size="2.5rem"
+              onClick={() => history.push("/settings")}
+              style={{ cursor: "pointer" }}
+            />
+          </Box>
+          <Box w="40px" h="40px">
+            <GoGraph size="2.5rem" style={{ cursor: "not-allowed" }} />
+          </Box>
+          <Box w="40px" h="40px">
+            <FiSettings size="2.5rem" style={{ cursor: "not-allowed" }} />
+          </Box>
+        </VStack>
+
+        <Box pos="fixed" bottom="3%" w="5%" centerContent>
+          <Link to="/login">
+            Login <FiExternalLink style={{ display: "inline" }} />
+          </Link>
+        </Box>
+      </Container>
     </>
   );
 };
